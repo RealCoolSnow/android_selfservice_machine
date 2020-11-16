6eb1790a542a949ce82a366945d02aef5742fd5c
CoolSnow <coolsnow2020@gmail.com>
2020-11-16 16:59:41 +0800

    update

---
+++ app/src/main/java/app/selfservice/machine/Application.java
@@ -0,0 +1,15 @@
+package app.selfservice.machine;
+
+
+import com.orhanobut.logger.Logger;
+
+import app.tv.logger.LoggerConfig;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LoggerConfig.init(this, Constants.LOGGER_TAG, Constants.DEBUG);
+        Logger.i("app created");
+    }
+}
---
+++ app/src/main/java/app/selfservice/machine/activity/MainActivity.java
@@ -0,0 +1,42 @@
+package app.selfservice.machine.activity;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import app.tv.network.BaseObserver;
+import app.tv.network.RxScheduler;
+import app.tv.network.bean.BaseResp;
+import app.selfservice.machine.api.bean.req.HelloReq;
+import app.selfservice.machine.api.bean.resp.HelloResp;
+import app.selfservice.machine.base.BaseActivity;
+import app.selfservice.machine.databinding.ActivityMainBinding;
+import app.selfservice.machine.network.RetrofitFactory;
+import io.reactivex.Observable;
+
+
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    @Override
+    protected void initView() {
+        binding.btnAppList.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, AppInstalledActivity.class));
+        });
+        binding.btnHttpTest.setOnClickListener(view -> httpTest());
+    }
+
+    private void httpTest() {
+        HelloReq helloReq = new HelloReq();
+        Observable<BaseResp<HelloResp>> observable = RetrofitFactory.getInstance().hello(helloReq.toRequestBody());
+        observable.compose(RxScheduler.compose(this.bindToLifecycle())).subscribe(new BaseObserver<HelloResp>(this) {
+            @Override
+            protected void onSuccess(String msg, HelloResp data) {
+                Toast.makeText(MainActivity.this, data.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected void onError(int code, String msg) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
